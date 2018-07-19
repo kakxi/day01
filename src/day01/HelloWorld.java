1f6725451df9c214cd796c5f2c22d91cffc3d374
kakxi <276983381@qq.com>
2018-07-19 10:29:44 +0800

    tijiaowejian

---
+++ src/day01/HelloWorld.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Java,HelloWorld!");
+
+		System.out.println(false);
+
+	}
+
+}
---
+++ src/day01/Test.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		byte[] bytes=null;
+		for (int i = 0; i < 100; i++)
+		{
+			bytes = new byte[1024*1024];
+		}
+	}
+}
